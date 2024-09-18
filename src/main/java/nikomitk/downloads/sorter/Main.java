1588481018c530d0fecb5e4c9ed8b6d481c62c4e
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-09-18 18:46:11 +0200

    Logging configuration

---
+++ src/main/java/nikomitk/downloads/sorter/Main.java
@@ -0,0 +1,14 @@
+package nikomitk.downloads.sorter;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    static final Logger LOG = LogManager.getLogger("Download-Sorter");
+
+    public static void main(String[] args) {
+
+    }
+}
